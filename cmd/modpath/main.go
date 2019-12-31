00bd63e47dd0c82a75d284ef4c532a265394a968
nakabonne <nakabonne@gmail.com>
2019-12-31 12:21:40 +0900

    Allow multiple args

--- cmd/modpath/main.go
+++ cmd/modpath/main.go
@@ -3,17 +3,38 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
 
 	"github.com/nakabonne/modpath"
 )
 
 func main() {
-	flag.Parse()
-	dir := flag.Arg(0)
-	modPath, err := modpath.Run(dir)
-	if err != nil {
-		fmt.Println("failed to find the module path: ", err)
-		return
+	os.Exit(main1())
+}
+
+func main1() int {
+	flag.Usage = func() {
+		fmt.Fprintln(os.Stderr, "usage: modpath [directories...]")
+		flag.PrintDefaults()
 	}
-	fmt.Println(modPath)
+	flag.Parse()
+
+	dirs := flag.Args()
+	if len(dirs) == 0 {
+		dirs = append(dirs, ".")
+	}
+
+	paths := make([]string, 0, len(dirs))
+	for _, dir := range dirs {
+		path, err := modpath.Run(dir)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "failed to find the module path:", err)
+			return 1
+		}
+		paths = append(paths, path)
+	}
+	for _, p := range paths {
+		fmt.Println(p)
+	}
+	return 0
 }
